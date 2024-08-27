8340d44fe01d6a7ca9da2e09cf0f76a335534992
Luis Garcia <luis200107@icloud.com>
2024-08-26 22:46:15 -0600

    Añadiendo comentarios a Tarea Uno de Programación Web 2

--- ProgramacionWeb2/TareaUno/app.js
+++ ProgramacionWeb2/TareaUno/app.js
@@ -1,2 +1,5 @@
+//Variable en la que se ingresa el nombre del usuario
 let nombre = prompt("Ingresa tu nombre: ");
+//Alert para mostrar un cuadro de texto con el nombre
+//del usuario
 alert("Tu nombre es: " + nombre);
