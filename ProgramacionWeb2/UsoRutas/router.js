b7be713aaf16fce3d208b2c1310670c4bc4c8d6e
Luis Garcia <luis200107@icloud.com>
2024-10-28 22:59:55 -0600

    Añadiendo uso de rutas en Node con Express

--- ProgramacionWeb2/UsoRutas/router.js
+++ ProgramacionWeb2/UsoRutas/router.js
@@ -1,18 +1,27 @@
+/*requier()
+Obtiene modulos de JavaScript con la extensión .js, mientras que
+import los obtiene de las extensiones .mjs. Obtiene las definiciones
+del modulo de express*/
 const express = require('express');
+/*Obtine las herramientas del modulo de path*/
 const path = require('path');
+/*express.Router crea un nuevo objeto Router
+Es una mini aplicación enficada en middleware y funciones de routing,
+Este objeto actua como middleware en si mismo por por que puede ser
+usado como parametro en una función que use middleware*/
 const router = express.Router();
 
+/*Métodos HTTP que maneja la ruta del objeto Router de express*/
 router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '/templates', 'index.html')); // Asegúrate de que la ruta sea correcta
+    res.sendFile(path.join(__dirname, '/templates', 'index.html')); //Función para get
 });
 
 router.get('/formulario', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '/templates', 'formulario.html')); // Asegúrate de que la ruta sea correcta
+    res.sendFile(path.join(__dirname, '/templates', 'formulario.html')); //Función para get
 });
 
 router.get('/informacion', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '/templates', 'informacion.html')); // Asegúrate de que la ruta sea correcta
+    res.sendFile(path.join(__dirname, '/templates', 'informacion.html')); //Función para get
 });
-
-
+/*Exporta modulos en JS*/
 module.exports = router;
--- ProgramacionWeb2/UsoRutas/server.js
+++ ProgramacionWeb2/UsoRutas/server.js
@@ -1,13 +1,21 @@
+/*Obtiene las herramientas del modulo de express*/
 const express = require("express");
+/*express()
+crea una aplicación de express. contiene herramientas
+para manear peticiones http*/
 const app = express();
-const router = require("./router");
+/*Obtine las herramientas del modulo de router.js*/
+const router = require("./router.js");
 const port = 3006;
-
+const path = require('path');
+app.use(express.static("./"));
 // Usa el enrutador con la aplicación Express
-//Usa el Middleware
+//use() Usa el Middleware dado
 app.use("/", router);
-
-// Escucha el puerto con Express directamente
+// Escucha el puerto con Express directamente, es identio a Node.http.listen()
+//Se ejecuta cuando se monta un servidor
 app.listen(port, () => {
+    console.log('Rutas')
+    console.log(path.join(__dirname, '/css'));
     console.log(`El servidor está en http://localhost:${port}`);
 });
